4bdafa9245351ea01296a1e80f8a23240d18c287
Leonard Martin <leonard.martin@gmail.com>
2018-03-27 16:09:40 +0100

    Add title config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  title: process.env_BUILD_MONITOR_TITLE
+  title: process.env_BUILD_MONITOR_TITLE,
   team: process.env.GITHUB_TEAM,
   token: process.env.GITHUB_TOKEN
 }
