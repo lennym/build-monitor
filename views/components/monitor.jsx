138cdb1ccb0a5c99bc93321cf3d97973f2dc10d5
Leonard Martin <leonard.martin@gmail.com>
2018-03-27 16:10:01 +0100

    Sort repos by states and PRs

--- views/components/monitor.jsx
+++ views/components/monitor.jsx
@@ -21,11 +21,30 @@ class Monitor extends React.Component {
     });
   }
 
+  sort(repos) {
+    if (!this.state) {
+      return repos;
+    }
+    return repos.sort((a, b) => {
+      const stateA = this.state[a.name];
+      const stateB = this.state[b.name];
+      if (stateA.build === stateB.build) {
+        return stateB.pull_requests.length - stateA.pull_requests.length;
+      }
+      if (this.state[a.name].build === 'failed') {
+        return -1;
+      }
+      if (this.state[a.name].build === 'unknown') {
+        return 1;
+      }
+    });
+  }
+
   render() {
     const repos = this.props.repos || [];
     return <React.Fragment>
       {
-        repos.map(repo => {
+        this.sort(repos).map(repo => {
           const state = this.state && this.state[repo.name] ? this.state[repo.name] : {}
           return <Repo
             name={repo.name}
